48566e9b4a70c5a842becf431354f2b441a59703
castynet <riungemaina.gmail.com>
2020-09-21 18:22:43 +0300

    index: added context provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ApiProvider } from "./components/context";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider>
+      <App />
+    </ApiProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 serviceWorker.unregister();
