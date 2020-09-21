0fba687eecf13bc42bf66c4ac796061940a78870
castynet <riungemaina.gmail.com>
2020-09-21 18:23:36 +0300

    components/urls: created & added some basic google urls

---
+++ src/components/urls.js
@@ -0,0 +1,6 @@
+export default {
+    google: {
+        getCalendars: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+        getEvents: 'https://www.googleapis.com/calendar/v3/calendars/#{calendarId}/events',
+    },
+};
