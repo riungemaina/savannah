e95f17bf5d367986559a0acf278c3770a81060f4
castynet <riungemaina.gmail.com>
2020-09-22 22:45:51 +0300

    components/utilities: created & configured links and styles

---
+++ src/components/utilities.js
@@ -0,0 +1,20 @@
+export const styles = {
+  fonts: {
+    titleFont: `font-family: "Crimson Text", serif`,
+    bodyFont: `font-family: 'Roboto', sans-serif;`,
+  },
+  deviceSize: {
+    mobile: `@media (max-width: 425px)`,
+    tablet: `@media (max-width: 768 px)`,
+    desktop: `@media (max-width: 1440 px)`,
+  },
+};
+
+export const urls = {
+  google: {
+    getCalendars:
+      "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+    getEvents:
+      "https://www.googleapis.com/calendar/v3/calendars/#{calendarId}/events",
+  },
+};
