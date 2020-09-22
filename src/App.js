e62d7446a4e57bca209f43b42f672e9e9d577a7f
castynet <riungemaina.gmail.com>
2020-09-22 22:43:31 +0300

    App: configured conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Auth from "./components/auth.jsx";
+import { useApi } from "./components/context";
+import LoadingCalender from "./components/loadingCalender";
 
 function App() {
+  const isSignedIn = useApi().isSignedInState;
+  const pageView = useApi().pageViewState;
+
+  function ShowPage() {
+    if (isSignedIn) {
+      switch (pageView) {
+        case "Loading":
+          return <LoadingCalender />;
+        case "ShowCalender":
+          return <button>Login</button>;
+        case "EditCalender":
+          return <button>Login</button>;
+        default:
+          return <p>Error page</p>;
+      }
+    }
+    return <Auth />;
+  }
+
   return (
     <>
-      <Auth />
+      <ShowPage />
     </>
   );
 }
