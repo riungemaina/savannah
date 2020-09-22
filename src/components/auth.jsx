3ff466949503ec7ff70ce87dfb2b6231f5f71268
castynet <riungemaina.gmail.com>
2020-09-22 22:44:50 +0300

    components/auth: wrapped up, with general functionality, layout & styles

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,27 +1,78 @@
 import React from "react";
 import { useApi } from "./context";
 import styled from "styled-components";
+import { styles } from "./utilities";
+import { Google } from "@styled-icons/boxicons-logos/Google";
 
+export default function LoginScreen() {
+  const handleLogin = useApi().signIn;
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>ACK St Peters Academy</Title>
+        <Description>School Timetable</Description>
+        <Button onClick={handleLogin}>
+          Login
+          <Google size="30" />
+        </Button>
+      </Wrapper>
+    </Container>
+  );
+}
+
+// Styled Components Styles
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   height: 100vh;
-  transform: translateY(-2%);
 `;
 
-const LoginScreen = () => {
-  const handleLogin = useApi().signIn;
-  const handleIsIn = useApi().isSignedInState;
+const Wrapper = styled.div`
+  padding: 40px 30px;
+  text-align: center;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.24);
+  background: #ddf;
+  border-radius: 2px;
 
-  return (
-    <Container>
-      <h1>Calendar Web Client</h1>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleIsIn}>Login</button>
-    </Container>
-  );
-};
+  ${styles.deviceSize.mobile} {
+    padding: 40px 0;
+    max-width: 95%;
+    font-size: 15px;
+  }
+`;
 
-export default LoginScreen;
+const Title = styled.h1`
+  ${styles.fonts.titleFont};
+  font-size: 3em;
+`;
+
+const Description = styled.h3`
+  ${styles.fonts.bodyFont};
+  font-size: 2em;
+  padding: 20px 0;
+  letter-spacing: 2px;
+`;
+
+const Button = styled.button`
+  width: 75%;
+  ${styles.fonts.bodyFont};
+  margin: 30px 0 0 0;
+  font-size: 1.5em;
+  background: #4caf50;
+  border: 0;
+  color: white;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  gap: 20px;
+  letter-spacing: 2px;
+
+  &:hover {
+    cursor: pointer;
+    background: #2c8f30;
+  }
+`;
