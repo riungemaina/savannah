58d2634ec3b2398f8c960a80d7bb9cf4eb0f8b8e
castynet <riungemaina.gmail.com>
2020-09-24 09:29:51 +0300

    components/context: updated authentication from gapi.auth2 to gapi.client

--- src/components/context.jsx
+++ src/components/context.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { urls } from "./utilities";
-// import { cloneDeep } from "lodash";
 
 const ApiContext = React.createContext();
 const ApiUpdateContext = React.createContext();
@@ -17,7 +16,7 @@ export function ApiProvider({ children }) {
   // Application States
   const [authState, setAuthState] = useState(null);
   const [isSignedInState, setIsSignedInState] = useState(false);
-  // const [user, setUser] = useState(null);
+  const [pageViewState, setPageViewState] = useState(null);
 
   window.startGapi = function () {
     window.gapi.load("client:auth2", init);
@@ -25,19 +24,49 @@ export function ApiProvider({ children }) {
 
   const init = async () => {
     try {
-      const auth = await window.gapi.auth2.init({
+      const auth = window.gapi.client.init({
+        apiKey: "AIzaSyA5oVlePqwpNPmxYVSgp8vrH3LeiTGiJMc",
         clientId:
           "289048714237-ruictr0hr3ajo3qgpe3tlugcpajak8i7.apps.googleusercontent.com",
+        discoveryDocs: [
+          "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+        ],
         scope: "https://www.googleapis.com/auth/calendar",
       });
+      window.gapi.auth2
+        .getAuthInstance()
+        .isSignedIn.listen((isSignedIn) => updateSignInStatus(isSignedIn));
 
-      auth.isSignedIn.listen((isSignedIn) => updateSignInStatus(isSignedIn));
-      updateSignInStatus(auth.isSignedIn.get());
-
+      updateSignInStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
       setAuthState(auth);
     } catch (e) {
       throw new Error(`Cannot connect with google calendar API. ${e}`);
     }
+  };
+
+  const loadingCalender = async () => {
+    const calenders = await getCalendarsList();
+    const SchoolCalender = calenders.find(
+      ({ summary }) => summary === "ACK St Peters Timetable"
+    );
+
+    if (SchoolCalender == null) {
+      createCalender();
+    } else {
+      console.log(SchoolCalender);
+      setPageViewState("ShowCalender");
+    }
+  };
+
+  const createCalender = async () => {
+    await window.gapi.client.calendar.calendars.insert({
+      resource: {
+        summary: "ACK St Peters Timetable",
+        description: "School Timetable",
+        timezone: "Africa/Nairobi",
+      },
+    });
+    loadingCalender();
   };
 
   const updateSignInStatus = (isSignedIn) => {
@@ -45,12 +74,12 @@ export function ApiProvider({ children }) {
   };
 
   const signIn = async () => {
-    await authState.signIn();
+    await window.gapi.auth2.getAuthInstance().signIn();
   };
 
   const logOut = async () => {
-    await authState.signOut();
-    updateSignInStatus(authState.isSignedIn.get());
+    await window.gapi.auth2.getAuthInstance().signOut();
+    updateSignInStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
   };
 
   const getCalendarsList = async () => {
@@ -91,18 +120,9 @@ export function ApiProvider({ children }) {
     }
   };
 
-  const getAuthInstance = async () => {
-    return await window.gapi.auth2.getAuthInstance();
-  };
-
-  const getCurrentUser = async () => {
-    const authInstance = await getAuthInstance();
-    return authInstance.currentUser;
-  };
-
   const getToken = async () => {
-    const user = await getCurrentUser();
-    const { token_type, access_token } = user.le.wc;
+    const authInstance = await window.gapi.auth2.getAuthInstance();
+    const { token_type, access_token } = authInstance.currentUser.le.wc;
     return [token_type, access_token];
   };
 
@@ -115,6 +135,8 @@ export function ApiProvider({ children }) {
         getCalendarsList,
         getEvents,
         makeRequest,
+        pageViewState,
+        loadingCalender,
       }}
     >
       {/* <ApiUpdateContext> */} {children} {/* </ApiUpdateContext> */}{" "}
