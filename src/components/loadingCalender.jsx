996acb9568331187f3f74462f05ec005854cffa7
castynet <riungemaina.gmail.com>
2020-09-24 09:32:21 +0300

    components/loadingCalender: styled and added a function call to update calender

--- src/components/loadingCalender.jsx
+++ src/components/loadingCalender.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled, { keyframes } from "styled-components";
+import { Settings } from "@styled-icons/feather/Settings";
+import { styles } from "./utilities";
 import { useApi } from "./context";
 
 export default function LoadingCalender() {
-  const handleLogout = useApi().logOut;
-  const getCalender = useApi().getCalendarsList();
-  console.log(getCalender)
+  const loadCalender = useApi().loadingCalender;
+
+  useEffect(() => {
+    loadCalender();
+  });
+
   return (
     <>
-      <p>Loading Calender</p>
-      <button onClick={handleLogout}>Logout</button>
+      <Container>
+        <Rotate>
+          <Settings size="70" />
+        </Rotate>
+        <Description>Loading Calender...</Description>
+      </Container>
     </>
   );
 }
+
+const Description = styled.p`
+  ${styles.fonts.bodyFont};
+  font-size: 2em;
+  padding: 20px 0;
+  letter-spacing: 2px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 90vh;
+`;
+
+const Rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+`;
+
+const Rotate = styled.div`
+  width: fit-content;
+  animation: ${Rotation} 2s infinite linear;
+`;
