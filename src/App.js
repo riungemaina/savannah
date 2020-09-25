5814766acc73f23d6061af545e6b9dafa37dfc7b
castynet <riungemaina.gmail.com>
2020-09-25 10:45:10 +0300

    App: updated the render logic

--- src/App.js
+++ src/App.js
@@ -3,37 +3,38 @@ import Auth from "./components/auth.jsx";
 import { useApi } from "./components/context";
 import LoadingCalender from "./components/loadingCalender";
 import Nav from "./components/nav";
+import Timetable from "./components/timetable";
 
 function App() {
   const isSignedIn = useApi().isSignedInState;
   const pageView = useApi().pageViewState;
 
-  function ShowPage() {
-    if (isSignedIn) {
-      return (
-        <>
-          <Nav />
-          <ShowView />
-        </>
-      );
-    }
-    return <Auth />;
-  }
+  // function ShowPage() {
+  //   if (isSignedIn) {
+  //     return (
+  //       <>
+  //         <Nav />
+  //         <ShowView />
+  //       </>
+  //     );
+  //   }
+  //   return <Auth />;
+  // }
 
-  function ShowView() {
-    switch (pageView) {
-      case "ShowCalender":
-        return <button>Login</button>;
-      case "EditCalender":
-        return <button>Login</button>;
-      default:
-        return <LoadingCalender />;
-    }
-  }
+  // function ShowView() {
+  //   switch (pageView) {
+  //     case "ShowCalender":
+  //       return <Timetable />;
+  //     default:
+  //       return <LoadingCalender />;
+  //   }
+  // }
 
   return (
     <>
-      <ShowPage />
+      {/* <ShowPage /> */}
+      <Nav />
+      <Timetable />
     </>
   );
 }
