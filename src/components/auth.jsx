63e136939f5e8d7b570ca60fbda065815642c354
castynet <riungemaina.gmail.com>
2020-09-27 12:26:43 +0300

    components/auth: removed bg image

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -3,28 +3,28 @@ import { useApi } from "./context";
 import styled from "styled-components";
 import { styles } from "./utilities";
 import { Google } from "@styled-icons/boxicons-logos/Google";
-import bgImg from "./Static/background-01.png"
 
 export default function LoginScreen() {
   const handleLogin = useApi().signIn;
 
   return (
-    <Container>
-      <Wrapper>
-        <Title>ACK St Peters Academy</Title>
-        <Description>School Timetable</Description>
-        <Button onClick={handleLogin}>
-          Login
-          <Google size="30" />
-        </Button>
-      </Wrapper>
-    </Container>
+    <>
+      <Container>
+        <Wrapper>
+          <Title>ACK St Peters Academy</Title>
+          <Description>School Timetable</Description>
+          <Button onClick={handleLogin}>
+            Login
+            <Google size="30" />
+          </Button>
+        </Wrapper>
+      </Container>
+    </>
   );
 }
 
 // Styled Components Styles
 const Container = styled.div`
-  background-image: url(${bgImg});
   display: flex;
   justify-content: center;
   align-items: center;
@@ -76,5 +76,6 @@ const Button = styled.button`
   &:hover {
     cursor: pointer;
     background: #2c8f30;
+    box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.24);
   }
 `;
