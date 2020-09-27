d9f3bfc20a7f5cb8a0e0eb7bf460796295f5386e
castynet <riungemaina.gmail.com>
2020-09-27 12:22:26 +0300

    App: updated render logic

--- src/App.js
+++ src/App.js
@@ -4,37 +4,40 @@ import { useApi } from "./components/context";
 import LoadingCalender from "./components/loadingCalender";
 import Nav from "./components/nav";
 import Timetable from "./components/timetable";
+import AddRecord from "./components/addRecord";
 
 function App() {
   const isSignedIn = useApi().isSignedInState;
   const pageView = useApi().pageViewState;
 
-  // function ShowPage() {
-  //   if (isSignedIn) {
-  //     return (
-  //       <>
-  //         <Nav />
-  //         <ShowView />
-  //       </>
-  //     );
-  //   }
-  //   return <Auth />;
-  // }
+  function ShowPage() {
+    if (isSignedIn) {
+      return (
+        <>
+          <Nav />
+          <ShowView />
+        </>
+      );
+    }
+    return <Auth />;
+  }
 
-  // function ShowView() {
-  //   switch (pageView) {
-  //     case "ShowCalender":
-  //       return <Timetable />;
-  //     default:
-  //       return <LoadingCalender />;
-  //   }
-  // }
+  function ShowView() {
+    switch (pageView) {
+      case "Create New Lesson":
+        return <Timetable />;
+      case "Show Timetable":
+        return <AddRecord />;
+      default:
+        return <LoadingCalender />;
+    }
+  }
 
   return (
     <>
-      {/* <ShowPage /> */}
-      <Nav />
-      <Timetable />
+      <ShowPage />
+      {/* <Nav />
+      <Timetable /> */}
     </>
   );
 }
