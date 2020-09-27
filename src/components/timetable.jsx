d0e8b44a0734b6883ad7ed7081473b8d3832d27c
castynet <riungemaina.gmail.com>
2020-09-27 12:24:39 +0300

    components/timetable: added component logic & styled

--- src/components/timetable.jsx
+++ src/components/timetable.jsx
@@ -1,31 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { styles } from "./utilities";
-// import Alert from "./alert"
-// import AddRecord from "./addRecord"
+import { useApi } from "./context";
+
 
 export default function LoadingCalender() {
+  const [lesson, setLesson] = useState(null);
+
+  useEffect(() => {
+    loadLesson().then(function (result) {
+      setLesson(result);
+    });
+  }, []);
+
+  const loadLesson = useApi().getEvents;
+  function classTime(time) {
+    // the time returned by google is in UTC(0-z)
+    // so we -3 to get the time in Africa/Nairobi
+
+    if (/05:00/.test(time)) {
+      return "8:00AM - 10:00AM";
+    } else if (/07:00/.test(time)) {
+      return "10:00AM - 12:00PM";
+    } else if (/09:00/.test(time)) {
+      return "12:00PM - 12:00AM";
+    }
+  }
+
+  function weekDay(date) {
+    const day = new Date(date).getDay();
+    return day === 1
+      ? "Monday"
+      : day === 2
+      ? "Teusday"
+      : day === 3
+      ? "Wednesday"
+      : day === 4
+      ? "Thursday"
+      : day === 5
+      ? "Friday"
+      : "hmm🤔, seems to have an error";
+  }
+
+  function ShowLessons() {
+    if (lesson !== null) {
+      return (
+        <>
+          {lesson.map((lesson) => (
+            <div key={lesson.id}>
+              <Wrapper>
+                <Title>{lesson.summary}</Title>
+                <Teacher>Teacher - {lesson.description}</Teacher>
+                <Subject>
+                  Subject - {lesson.extendedProperties.shared.subject}
+                </Subject>
+                <Weekday>Day - {weekDay(lesson.start.dateTime)}</Weekday>
+                <Time>Time - {classTime(lesson.start.dateTime)}</Time>
+              </Wrapper>
+            </div>
+          ))}
+        </>
+      );
+    }
+    return <p>loading</p>;
+  }
+
+  console.log(lesson);
+
   return (
     <>
-      {/* <Alert />
-      <AddRecord/> */}
-      <Wrapper>
-        <Title>Class 1</Title>
-        <Teacher>Teacher - Ms Alice</Teacher>
+      {/* <Alert /> */}
 
-        <Subject>Subject - Science</Subject>
-
-        <Weekday>Day - Monday</Weekday>
-
-        <Time>Time - 8:00 AM - 10 AM</Time>
-      </Wrapper>
+      <ShowLessons />
     </>
   );
 }
 const Wrapper = styled.div`
-  ${styles.fonts.titleFont};
   letter-spacing: 2px;
-    max-width: 18%;
+  max-width: 20%;
   margin: 2%;
   border: 1px solid #008eff;
   background-color: #008eff;
@@ -40,9 +92,10 @@ const Wrapper = styled.div`
   }
   &:hover h2 { color: #000; border-bottom: 1px solid #008eff;}
 
-  p{ 
+  p{
     padding 10px; 
     background-color: #fff;
+      ${styles.fonts.bodyFont};
   }
 
     ${styles.deviceSize.mobile} {
@@ -60,6 +113,7 @@ const Title = styled.h2`
   text-align: center;
   color: white;
   transition: color 0.28s ease;
+  ${styles.fonts.titleFont};
 `;
 const Teacher = styled.p`
   border-radius: 10px 10px 0 0;
