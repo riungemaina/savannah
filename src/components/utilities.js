062f60f868fc76de2f3d9094fb9cb9c9503014b1
castynet <riungemaina.gmail.com>
2020-09-30 16:04:43 +0300

    components/utilities: added the extra GAPI urls

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -12,9 +12,9 @@ export const styles = {
 
 export const urls = {
   google: {
-    getCalendars:
-      "https://www.googleapis.com/calendar/v3/users/me/calendarList",
-    getEvents:
-      "https://www.googleapis.com/calendar/v3/calendars/#{calendarId}/events",
+    getCalendars: "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+    getEvents: "https://www.googleapis.com/calendar/v3/calendars/#{calendarId}/events",
+    discoveryDocs: "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    scope: "https://www.googleapis.com/auth/calendar",
   },
 };
