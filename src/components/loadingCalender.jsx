404e71aeaea11c884fe13f5a77febef52a0fdcb7
castynet <riungemaina.gmail.com>
2020-09-30 16:03:28 +0300

    components/loadingCalender: re-styled

--- src/components/loadingCalender.jsx
+++ src/components/loadingCalender.jsx
@@ -9,25 +9,46 @@ export default function LoadingCalender() {
 
   useEffect(() => {
     loadCalender();
-  });
+  }, []);
 
   return (
     <>
       <Container>
-        <Rotate>
-          <Settings size="70" />
-        </Rotate>
-        <Description>Loading Calender...</Description>
+        <Wrapper>
+          <Rotate>
+            <Settings size="70" />
+          </Rotate>
+          <Description>
+            Hang tight,
+            <span role="img" aria-label="smile">
+              😊
+            </span>
+            <br /> we're loading your calender...
+          </Description>
+        </Wrapper>
       </Container>
     </>
   );
 }
+const Wrapper = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: rgb(16 16 48 / 0.95);
+  padding: 50px;
+  border-radius: 5px;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.24);
+  color: #fff;
+`;
 
 const Description = styled.p`
   ${styles.fonts.bodyFont};
-  font-size: 2em;
+  font-size: 1.6em;
   padding: 20px 0;
   letter-spacing: 2px;
+  text-align: center;
 `;
 
 const Container = styled.div`
